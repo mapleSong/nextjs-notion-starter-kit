b39d9d7cf1f91cdbc1ce7e837783b4ba97b89215
mapleSong <meetguardian@gmail.com>
2021-12-31 16:17:00 +0800

    feat: update site config

--- site.config.js
+++ site.config.js
@@ -7,19 +7,19 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'maple blog',
-  domain: 'maple.it',
-  author: 'maple',
+  name: 'Yu Ming',
+  domain: 'vcodes.me',
+  author: 'YuMing',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: '',
+  socialImageTitle: '',
+  socialImageSubtitle: '',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
